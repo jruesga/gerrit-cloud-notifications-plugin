c6e889d5a3ac26f13a03a8b084b59978309a668c
Jorge Ruesga <jorge@ruesga.com>
2016-11-26 22:11:54 +0100

    Never crash processing an event
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/main/java/com/ruesga/gerrit/plugins/fcm/handlers/EventHandler.java
+++ src/main/java/com/ruesga/gerrit/plugins/fcm/handlers/EventHandler.java
@@ -19,8 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gerrit.common.EventListener;
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.server.data.AccountAttribute;
 import com.google.gerrit.server.data.ChangeAttribute;
 import com.google.gerrit.server.data.PatchSetAttribute;
@@ -42,85 +45,98 @@ import com.ruesga.gerrit.plugins.fcm.workers.FcmUploaderWorker;
 
 public class EventHandler implements EventListener {
 
+    private static final Logger log =
+            LoggerFactory.getLogger(EventHandler.class);
+
+    private final String pluginName;
     private final FcmUploaderWorker uploader;
 
     @Inject
-    public EventHandler(FcmUploaderWorker uploader) {
+    public EventHandler(
+            @PluginName String pluginName,
+            FcmUploaderWorker uploader) {
         super();
+        this.pluginName = pluginName;
         this.uploader = uploader;
     }
 
 
     @Override
     public void onEvent(Event event) {
-        int eventId;
-        ChangeAttribute change;
-        AccountAttribute author = null;
-        PatchSetAttribute patchset = null;
-        String message = null;
-        if (event instanceof ChangeAbandonedEvent) {
-            eventId = CloudNotificationEvents.CHANGE_ABANDONED_EVENT;
-            change = ((ChangeAbandonedEvent) event).change.get();
-            author = ((ChangeAbandonedEvent) event).abandoner.get();
-            patchset = ((ChangeAbandonedEvent) event).patchSet.get();
-        } else if (event instanceof ChangeMergedEvent) {
-            eventId = CloudNotificationEvents.CHANGE_MERGED_EVENT;
-            change = ((ChangeMergedEvent) event).change.get();
-            author = ((ChangeMergedEvent) event).submitter.get();
-            patchset = ((ChangeMergedEvent) event).patchSet.get();
-        } else if (event instanceof ChangeRestoredEvent) {
-            eventId = CloudNotificationEvents.CHANGE_RESTORED_EVENT;
-            change = ((ChangeRestoredEvent) event).change.get();
-            author = ((ChangeRestoredEvent) event).restorer.get();
-            patchset = ((ChangeRestoredEvent) event).patchSet.get();
-        } else if (event instanceof CommentAddedEvent) {
-            eventId = CloudNotificationEvents.COMMENT_ADDED_EVENT;
-            change = ((CommentAddedEvent) event).change.get();
-            author = ((CommentAddedEvent) event).author.get();
-            patchset = ((ChangeRestoredEvent) event).patchSet.get();
-        } else if (event instanceof DraftPublishedEvent) {
-            eventId = CloudNotificationEvents.DRAFT_PUBLISHED_EVENT;
-            change = ((DraftPublishedEvent) event).change.get();
-            author = ((DraftPublishedEvent) event).uploader.get();
-            patchset = ((DraftPublishedEvent) event).patchSet.get();
-        } else if (event instanceof HashtagsChangedEvent) {
-            eventId = CloudNotificationEvents.HASHTAG_CHANGED_EVENT;
-            change = ((HashtagsChangedEvent) event).change.get();
-            author = ((HashtagsChangedEvent) event).editor.get();
-        } else if (event instanceof ReviewerAddedEvent) {
-            eventId = CloudNotificationEvents.REVIEWER_ADDED_EVENT;
-            change = ((ReviewerAddedEvent) event).change.get();
-        } else if (event instanceof ReviewerDeletedEvent) {
-            eventId = CloudNotificationEvents.REVIEWER_DELETED_EVENT;
-            change = ((ReviewerDeletedEvent) event).change.get();
-        } else if (event instanceof PatchSetCreatedEvent) {
-            eventId = CloudNotificationEvents.PATCHSET_CREATED_EVENT;
-            change = ((PatchSetCreatedEvent) event).change.get();
-            author = ((PatchSetCreatedEvent) event).uploader.get();
-            patchset = ((PatchSetCreatedEvent) event).patchSet.get();
-        } else if (event instanceof TopicChangedEvent) {
-            eventId = CloudNotificationEvents.TOPIC_CHANGED_EVENT;
-            change = ((TopicChangedEvent) event).change.get();
-            author = ((TopicChangedEvent) event).changer.get();
-        } else {
-            // Unsupported event for FCM notifications
-            return;
+        try {
+            int eventId;
+            ChangeAttribute change;
+            AccountAttribute author = null;
+            PatchSetAttribute patchset = null;
+            String message = null;
+            if (event instanceof ChangeAbandonedEvent) {
+                eventId = CloudNotificationEvents.CHANGE_ABANDONED_EVENT;
+                change = ((ChangeAbandonedEvent) event).change.get();
+                author = ((ChangeAbandonedEvent) event).abandoner.get();
+                patchset = ((ChangeAbandonedEvent) event).patchSet.get();
+            } else if (event instanceof ChangeMergedEvent) {
+                eventId = CloudNotificationEvents.CHANGE_MERGED_EVENT;
+                change = ((ChangeMergedEvent) event).change.get();
+                author = ((ChangeMergedEvent) event).submitter.get();
+                patchset = ((ChangeMergedEvent) event).patchSet.get();
+            } else if (event instanceof ChangeRestoredEvent) {
+                eventId = CloudNotificationEvents.CHANGE_RESTORED_EVENT;
+                change = ((ChangeRestoredEvent) event).change.get();
+                author = ((ChangeRestoredEvent) event).restorer.get();
+                patchset = ((ChangeRestoredEvent) event).patchSet.get();
+            } else if (event instanceof CommentAddedEvent) {
+                eventId = CloudNotificationEvents.COMMENT_ADDED_EVENT;
+                change = ((CommentAddedEvent) event).change.get();
+                author = ((CommentAddedEvent) event).author.get();
+                patchset = ((ChangeRestoredEvent) event).patchSet.get();
+            } else if (event instanceof DraftPublishedEvent) {
+                eventId = CloudNotificationEvents.DRAFT_PUBLISHED_EVENT;
+                change = ((DraftPublishedEvent) event).change.get();
+                author = ((DraftPublishedEvent) event).uploader.get();
+                patchset = ((DraftPublishedEvent) event).patchSet.get();
+            } else if (event instanceof HashtagsChangedEvent) {
+                eventId = CloudNotificationEvents.HASHTAG_CHANGED_EVENT;
+                change = ((HashtagsChangedEvent) event).change.get();
+                author = ((HashtagsChangedEvent) event).editor.get();
+            } else if (event instanceof ReviewerAddedEvent) {
+                eventId = CloudNotificationEvents.REVIEWER_ADDED_EVENT;
+                change = ((ReviewerAddedEvent) event).change.get();
+            } else if (event instanceof ReviewerDeletedEvent) {
+                eventId = CloudNotificationEvents.REVIEWER_DELETED_EVENT;
+                change = ((ReviewerDeletedEvent) event).change.get();
+            } else if (event instanceof PatchSetCreatedEvent) {
+                eventId = CloudNotificationEvents.PATCHSET_CREATED_EVENT;
+                change = ((PatchSetCreatedEvent) event).change.get();
+                author = ((PatchSetCreatedEvent) event).uploader.get();
+                patchset = ((PatchSetCreatedEvent) event).patchSet.get();
+            } else if (event instanceof TopicChangedEvent) {
+                eventId = CloudNotificationEvents.TOPIC_CHANGED_EVENT;
+                change = ((TopicChangedEvent) event).change.get();
+                author = ((TopicChangedEvent) event).changer.get();
+            } else {
+                // Unsupported event for FCM notifications
+                return;
+            }
+    
+            List<String> notifiedUsers = obtainNotifiedUsers(change, author);
+    
+            Notification notification = new Notification();
+            notification.event = eventId;
+            notification.change = change.id;
+            notification.revision = patchset == null ? null : patchset.revision;
+            notification.project = change.project;
+            notification.branch = change.branch;
+            notification.topic = change.topic;
+            notification.author = author == null ? null : author.username;
+            notification.subject = StringUtils.abbreviate(change.subject, 100);
+            notification.message = message;
+    
+            this.uploader.notify(notifiedUsers, notification);
+
+        } catch (Exception ex) {
+            log.error(String.format(
+                    "[%s] Failed to process event", pluginName), ex);
         }
-
-        List<String> notifiedUsers = obtainNotifiedUsers(change, author);
-
-        Notification notification = new Notification();
-        notification.event = eventId;
-        notification.change = change.id;
-        notification.revision = patchset == null ? null : patchset.revision;
-        notification.project = change.project;
-        notification.branch = change.branch;
-        notification.topic = change.topic;
-        notification.author = author == null ? null : author.username;
-        notification.subject = StringUtils.abbreviate(change.subject, 100);
-        notification.message = message;
-
-        this.uploader.notify(notifiedUsers, notification);
     }
 
 
